f99e376f74105a32ede06d0c288295d32a651e5a
Vitaliy Naumenko <v.naumenko@wdss.com.ua>
2020-02-19 15:34:03 +0200

    added component to html

--- src/js/core/main.js
+++ src/js/core/main.js
@@ -1,6 +1,31 @@
 window.onload = function () {
+    let Movies = document.querySelector('#movies')
+    mainContent = '';
+    const requestUrl = new Request('http://my-json-server.typicode.com/moviedb-tech/movies/list');
 
-   console.log('ssss')
+    function sendRequest(url) {
+        return fetch(url)
+            .then(response => response.json())
+    }
+
+    sendRequest(requestUrl)
+        .then(films => {
+            console.log(films)
+            films.forEach((film) => {
+                console.log(film)
+                mainContent += `
+                <div class="movie col-lg-4">
+                   <img src="${film.img}">
+                         <h2 class="name movie__name">
+                             ${film.name}
+                        </h2>
+                        <div class="year movies__year">
+                            ${film.year}
+                        </div>
+                </div>`
+            })
+            Movies.innerHTML = mainContent
+        })
 
 }
 
